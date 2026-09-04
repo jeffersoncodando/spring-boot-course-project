bab905bf62e76d626ac2d91cfa91370d9dd3af7d
Jefferson Fernando <jefferson.fernandocg@gmail.com>
2026-09-03 20:34:39 -0400

    Project created

---
+++ src/test/java/com/jeffersoncodando/spring_boot_course_project/SpringBootCourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersoncodando.spring_boot_course_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
